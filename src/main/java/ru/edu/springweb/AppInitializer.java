93d7759791be45669d6582a8048dedbd3630f535
SuPeRKeNgUrA <luninartyom@yandex.ru>
2024-12-01 14:27:44 +0300

    Добавлен контроллер, html страницы и тесты

---
+++ src/main/java/ru/edu/springweb/AppInitializer.java
@@ -0,0 +1,27 @@
+package ru.edu.springweb;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import ru.edu.springweb.config.WebApplicationConfig;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(WebApplicationConfig.class);
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(WebApplicationConfig.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
